2492acad384c686bb1849e4fb74c71862e28604c
kumar <kumar.sendhi@gmail.com>
2020-08-16 17:14:16 +0530

    Publication Column data type changed and Cost column added

--- MyDatabase/Book.sql
+++ MyDatabase/Book.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Book ] NVARCHAR(50) NULL, 
     [Author] NVARCHAR(50) NULL, 
-    [Publication] NVARCHAR(50) NULL
+    [Publication] NVARCHAR(70) NULL, 
+    [Cost] DECIMAL NULL
 )
