43f0a210151037382882fc6ff0ccd42c31f05ac9
Kumar K <kumar.k@hp.com>
2021-04-15 19:09:23 +0530

    Last Name added as requested

--- MyDatabase/Employee.sql
+++ MyDatabase/Employee.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [First_Name] NVARCHAR(50) NOT NULL
+    [First_Name] NVARCHAR(50) NOT NULL, 
+    [Last_Name] NVARCHAR(50) NOT NULL
 )
