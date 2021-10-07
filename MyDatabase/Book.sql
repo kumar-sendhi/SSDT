5dd98b2ba7f9bc3dd7ea969d8912ba7903e2e956
Kumar Krishnamurthy <kumar.sendhi@hotmail.com>
2021-10-07 11:16:58 +0000

    Removed cost column

--- MyDatabase/Book.sql
+++ MyDatabase/Book.sql
@@ -3,8 +3,8 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Book ] NVARCHAR(50) NULL, 
     [Author] NVARCHAR(50) NULL, 
-    [Publication] NVARCHAR(70) NULL, 
-	[Cost] DECIMAL NULL
+    [Publication] NVARCHAR(70) NULL 
+	
    
 	
 )
